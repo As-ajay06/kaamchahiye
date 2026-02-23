92746c5c8cd14529f3dbf43cdf2b1520b664837a
Ajay Sahani <as2673447@gmail.com>
2026-02-24 01:35:55 +0530

    refactor: use env var for API URL

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,4 @@
-const API_URL = "https://kaamchahiye.onrender.com/";
-// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 
 export interface User {
